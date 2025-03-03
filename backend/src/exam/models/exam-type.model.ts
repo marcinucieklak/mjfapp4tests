90afb6342f03c4eb232cf611cd6714403102b45b
marcinucieklak <marcin.ucieklak@gmail.com>
2025-03-03 13:31:06 +0100

    Wersja produkcyjna V1.0A

--- backend/src/exam/models/exam-type.model.ts
+++
@@ -1,7 +0,0 @@
-import { Column, DataType, Model, Table } from 'sequelize-typescript';
-
-@Table({ tableName: 'exam_types' })
-export class ExamType extends Model {
-  @Column({ type: DataType.STRING, allowNull: false })
-  name: string; // 'Normal', 'By Topics', 'By Subtopics'
-}
